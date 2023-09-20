d9c8c34e602a4876b8c53037e9e29172fab9f222
Vamsi <krishnasaivamsi143@gmail.com>
2023-09-20 18:32:28 +0530

    added useState

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,13 +1,28 @@
-import React from "react";
+import React,{useState} from "react";
 import ResCard from "./ResCard";
 import RES_DATA from '../utils/constans'
 
+
+
 const Body = () => {
-    let restaurants = RES_DATA;
+    let restaurants1 = RES_DATA;
+    const [restaurants, setRestaurants] = useState(restaurants1);
+    console.log("setRestaurants1", setRestaurants1);
+    console.log("restaurants,", restaurants1);
+
+    const sortRatingabove4 = () => {
+        let dummyRate = []
+        dummyRate  = restaurants1.filter(item => {
+            console.log(item);
+            return item.info.avgRating > 4;
+        });
+        setRestaurants(dummyRate);
+    }
+
     return(
         <div className='body'>
-            <div className='search'> 
-                search
+            <div className='rating'> 
+                <button onClick={sortRatingabove4}>Highest Rating</button>
             </div>
             <div className='res-container'>
             {restaurants.map(item => <ResCard key={item.info.id} resData = {item}></ResCard>)}
--- src/Components/ResCard.js
+++ src/Components/ResCard.js
@@ -2,11 +2,11 @@ import React from 'react'
 import { CDN_URL } from '../utils/constans'
 
 const ResCard = (props) => {
-    console.log(props)
+    // console.log(props)
     const { name, cuisines, avgRating,cloudinaryImageId
     } = props.resData.info
     const {deliveryTime} = props.resData.info.sla
-    console.log("name",name)
+    // console.log("name",name)
     return(
         <div className='res-card'>
             <img className='res-logo' alt="rescard" src={CDN_URL+cloudinaryImageId}/>
